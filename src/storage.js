20563b571e6e1be5628daaada2998712af30cf29
jerry-weng <jjweng2007@gmail.com>
2016-09-07 18:12:29 +0800

    Add partitionsState to safely umount partitions and remove mountPoints

--- src/storage.js
+++ src/storage.js
@@ -1,10 +1,15 @@
 'use strict';
 
-var async = require('ruff-async');
-var spawn = require('child_process').spawn;
+var fs = require('fs');
 
 function Storage(blockInfo) {
     this._blockInfo = blockInfo;
+    this._partionsState = blockInfo.partitions.map(function () {
+        return {
+            unmounted: false,
+            removed: false
+        };
+    });
 }
 
 Object.defineProperties(Storage.prototype, {
@@ -17,8 +22,16 @@ Object.defineProperties(Storage.prototype, {
         get: function () {
             return this._blockInfo.partitions;
         }
+    },
+    isEjected: {
+        get: function () {
+            var res = true;
+            for (var i = 0; i < this._blockInfo.partitions.length; i++) {
+                res = res && this._partionsState[i].removed;
+            }
+            return res;
+        }
     }
-
 });
 
 Storage.prototype.getPartitionsByLabel = function (label) {
@@ -30,28 +43,54 @@ Storage.prototype.getPartitionsByLabel = function (label) {
     for (var i = 0; i < keyOfPartitions.length; i++) {
         var current = this._blockInfo.partitions[keyOfPartitions[i]];
         if (current.label === label) {
-            partitions.push(current.path);
+            partitions.push(current);
         }
     }
     return partitions;
 };
 
-Storage.prototype.eject = function (callback) {
-    var ejectPartitions = this._blockInfo.partitions.map(function (partition) {
-        return ejectPartition.bind(null, partition.path);
-    });
-    async.series(ejectPartitions, callback);
+Storage.prototype.eject = function () {
+    this._unmountPartitions();
+    this._removeMountPoints(true);
 };
 
-function ejectPartition(path, callback) {
-    var eject = spawn('umount', [path]);
-    eject.on('exit', function (code) {
-        if (code === 0) {
-            callback && callback();
-        } else {
-            callback && callback(new Error('Cannot eject the partition which path is', path));
+Storage.prototype._unmountPartitions = function () {
+    for (var i = 0; i < this._blockInfo.partitions.length; i++) {
+        var path = this._blockInfo.partitions[i].path;
+        if (!this._partionsState[i].unmounted) {
+            var res = uv.exec_sync('/bin/umount', [path]);
+            if (res) {
+                throw new Error('Cannot umount ' + path + ': Device or resource busy');
+            } else {
+                this._partionsState[i].unmounted = true;
+            }
         }
-    });
-}
+    }
+};
+
+Storage.prototype._removeMountPoints = function (checkUnmountState) {
+    for (var i = 0; i < this._blockInfo.partitions.length; i++) {
+        var path = this._blockInfo.partitions[i].path;
+        if (checkUnmountState) {
+            if (!this._partionsState[i].unmounted) {
+                throw new Error('The partition ' + path + ' is not ejected, please eject first');
+            }
+        }
+
+        if (!this._partionsState[i].removed) {
+            try {
+                fs.rmdirSync(path);
+                this._partionsState[i].removed = true;
+                this._partionsState[i].unmounted = true;
+            } catch (error) {
+                throw error;
+            }
+        }
+    }
+};
+
+Storage.prototype.cleanup = function () {
+    this._removeMountPoints(false);
+};
 
 module.exports = Storage;
